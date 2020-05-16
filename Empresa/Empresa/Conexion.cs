fffc5009b51e7ad71b87db36208770941f5d6a7b
Juan Felipe Avila Sanchez <pipeavila26@outlook.com>
2020-05-16 13:10:49 -0500

    Se armo la estructura del proyecto

---
+++ Empresa/Empresa/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace Empresa
+{
+    public static class Conexion
+    {
+        public static String Cadena_Conexion = ConfigurationManager.ConnectionStrings["EmpresaConnectionString"].ConnectionString;
+    }
+}
