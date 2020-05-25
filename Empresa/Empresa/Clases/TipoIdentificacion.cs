050a3e60258bf2c70d7e87238121e09dd72d9c96
Juan Felipe Avila Sanchez <pipeavila26@outlook.com>
2020-05-25 18:40:31 -0500

    Se agregaron los atributos y el costructor a ambas clases del proyecto

--- Empresa/Empresa/Clases/TipoIdentificacion.cs
+++ Empresa/Empresa/Clases/TipoIdentificacion.cs
@@ -7,5 +7,17 @@ namespace Empresa.Clases
 {
     public class TipoIdentificacion
     {
+        public int Identificacion_Id { get; set; }
+        public string Nombre { get; set; }
+
+        public TipoIdentificacion(int codigoIdentificacion,string nombre)
+        {
+            this.Identificacion_Id = codigoIdentificacion;
+            this.Nombre = nombre;
+        }
+        public TipoIdentificacion()
+        {
+
+        }
     }
 }
--- Empresa/Empresa/Clases/Trabajador.cs
+++ Empresa/Empresa/Clases/Trabajador.cs
@@ -7,5 +7,26 @@ namespace Empresa.Clases
 {
     public class Trabajador
     {
+        public int Trabajador_Id { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public int Identificacion { get; set; }
+        public int Identificador_Id { get; set; }
+        public decimal Salario { get; set; }
+
+        public Trabajador(int codigoTrabajador, string nombres, string apellidos, int identificacion, int codigoIdentificacion, decimal salario)
+        {
+            this.Trabajador_Id = codigoTrabajador;
+            this.Nombres = nombres;
+            this.Apellidos = apellidos;
+            this.Identificacion = identificacion;
+            this.Identificador_Id = codigoIdentificacion;
+            this.Salario = salario;
+        }
+
+        public Trabajador()
+        {
+
+        }
     }
 }
